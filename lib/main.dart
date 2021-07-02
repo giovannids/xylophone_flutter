971390eae2cda006643f02a5ecee2fc2fb8c7f27
Giovanni DS <g-io@MacBook-Air-de-Giovanni.local>
2021-07-02 13:05:09 -0600

    Completa la app. Hace uso de los tres tipos de funciones.

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,31 @@ import 'package:flutter/material.dart';
 // Importa el paquete audioplayers
 import 'package:audioplayers/audioplayers.dart';
 
+// función del primer tipo que no recibe ni devuelve ningún valor
 void main() {
   runApp(XylophoneApp());
 }
 
+// (fat) arrow function
+// void main() => runApp(XylophoneApp());
+
 class XylophoneApp extends StatelessWidget {
-  // usa una función que recibe un argumento o parámetro
+  // función del segundo tipo que recibe un argumento o parámetro
   void playSound(int soundNumber) {
     final player = AudioCache();
     player.play('note1.wav');
+  }
+
+  // función del tercer tipo que recibe valores y devuelve un valor
+  Expanded buildKey({required Color color, required int soundNumber}) {
+    return Expanded(
+      child: TextButton(
+        onPressed: () {
+          playSound(soundNumber);
+        },
+        child: Container(color: color),
+      ),
+    );
   }
 
   @override
@@ -20,67 +36,20 @@ class XylophoneApp extends StatelessWidget {
         body: SafeArea(
           child: Column(
             children: [
-              Expanded(
-                child: TextButton(
-                  onPressed: () {
-                    playSound(1);
-                  },
-                  child: Container(color: Colors.red),
-                ),
-              ),
-              Expanded(
-                child: TextButton(
-                  onPressed: () {
-                    playSound(2);
-                  },
-                  child: Container(color: Colors.blue),
-                ),
-              ),
-              Expanded(
-                child: TextButton(
-                  onPressed: () {
-                    playSound(3);
-                  },
-                  child: Container(color: Colors.orange),
-                ),
-              ),
-              Expanded(
-                child: TextButton(
-                  onPressed: () {
-                    playSound(4);
-                  },
-                  child: Container(color: Colors.green),
-                ),
-              ),
-              Expanded(
-                child: TextButton(
-                  onPressed: () {
-                    playSound(5);
-                  },
-                  child: Container(color: Colors.teal),
-                ),
-              ),
-              Expanded(
-                child: TextButton(
-                  onPressed: () {
-                    playSound(6);
-                  },
-                  child: Container(color: Colors.yellow),
-                ),
-              ),
-              Expanded(
-                child: TextButton(
-                  onPressed: () {
-                    playSound(7);
-                  },
-                  child: Container(color: Colors.purple),
-                ),
-              ),
+              buildKey(color: Colors.red, soundNumber: 1),
+              buildKey(color: Colors.blue, soundNumber: 2),
+              buildKey(color: Colors.orange, soundNumber: 3),
+              buildKey(color: Colors.green, soundNumber: 4),
+              buildKey(color: Colors.teal, soundNumber: 5),
+              buildKey(color: Colors.yellow, soundNumber: 6),
+              buildKey(color: Colors.purple, soundNumber: 7),
             ],
             // Propiedades complementarias
             crossAxisAlignment: CrossAxisAlignment.stretch,
           ),
         ),
+        // Propiedades complementarias
+        backgroundColor: Colors.black,
       ),
     );
   }
