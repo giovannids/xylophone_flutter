20e0e0f25011a0aad5bf9a06b010bc90288cbab6
Giovanni DS <g-io@MacBook-Air-de-Giovanni.local>
2021-07-02 11:06:12 -0600

    Agrega una función que recibe un argumento o parámetro

--- README.md
+++ README.md
@@ -2,15 +2,9 @@
 
 Proyecto hecho para el curso "The Complete 2021 Flutter Development Bootcamp with Dart"
 
-## Getting Started
+## Información breve y referencias
 
-This project is a starting point for a Flutter application.
+- [pub.dev: Find and use packages to build Dart and Flutter apps.](https://pub.dev/)
+- [AudioPlayers: A Flutter plugin to play multiple simultaneously audio files, works for Android, iOS, macOS and web.](https://pub.dev/packages/audioplayers)
 
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+Para más información ver la [documentación en línea](https://flutter.dev/docs).
--- lib/main.dart
+++ lib/main.dart
@@ -7,19 +7,78 @@ void main() {
 }
 
 class XylophoneApp extends StatelessWidget {
+  // usa una función que recibe un argumento o parámetro
+  void playSound(int soundNumber) {
+    final player = AudioCache();
+    player.play('note1.wav');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: TextButton(
-              onPressed: () {
-                final player = AudioCache();
-                player.play('note1.wav');
-              },
-              child: Text('Play'),
-            ),
+          child: Column(
+            children: [
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    playSound(1);
+                  },
+                  child: Container(color: Colors.red),
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    playSound(2);
+                  },
+                  child: Container(color: Colors.blue),
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    playSound(3);
+                  },
+                  child: Container(color: Colors.orange),
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    playSound(4);
+                  },
+                  child: Container(color: Colors.green),
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    playSound(5);
+                  },
+                  child: Container(color: Colors.teal),
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    playSound(6);
+                  },
+                  child: Container(color: Colors.yellow),
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    playSound(7);
+                  },
+                  child: Container(color: Colors.purple),
+                ),
+              ),
+            ],
+            // Propiedades complementarias
+            crossAxisAlignment: CrossAxisAlignment.stretch,
           ),
         ),
       ),
