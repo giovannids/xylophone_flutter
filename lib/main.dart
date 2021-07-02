7df4561a20cbbcf1fde439c67aa8db9c41b3cfcb
Giovanni DS <g-io@MacBook-Air-de-Giovanni.local>
2021-07-02 07:35:33 -0600

    Agrega y utiliza el paquete audioplayers

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+// Importa el paquete audioplayers
+import 'package:audioplayers/audioplayers.dart';
 
 void main() {
   runApp(XylophoneApp());
@@ -10,9 +12,29 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Center(
+            child: TextButton(
+              onPressed: () {
+                final player = AudioCache();
+                player.play('note1.wav');
+              },
+              child: Text('Play'),
+            ),
+          ),
         ),
       ),
     );
   }
 }
+
+/*
+NOTAS
+audioplayers
+add it to your class as a static member: static AudioCache player = AudioCache();
+or as a local variable: final player = AudioCache();
+
+REFERENCIAS
+https://pub.dev/
+https://pub.dev/packages/audioplayers
+https://github.com/luanpotter/audioplayers/blob/master/packages/audioplayers/doc/audio_cache.md
+ */
